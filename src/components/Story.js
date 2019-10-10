bac1596d437ceddac9e55a3fd8898f2e86ceb9ea
mikergarrity <mikergarrity@gmail.com>
2019-10-10 18:03:41 -0500

    added some styling

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { getStory } from '../services/hnApi';
+import {
+  StoryWrapper,
+  StoryTitle,
+  StoryMeta,
+  StoryMetaElement
+} from '../styles/StoryStyles';
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -8,12 +14,19 @@ export const Story = ({ storyId }) => {
     // eslint-disable-next-line
   }, []);
   return story && story.url ? (
-    <div>
-      <a href={story.url}>
-        <p>{story.title}</p>
-      </a>
-      By: <p>{story.by}</p>
-      Posted: <p>{story.time}</p>
-    </div>
+    <StoryWrapper data-testid='story'>
+      <StoryTitle>
+        <a href={story.url}>{story.title}</a>
+      </StoryTitle>
+      <StoryMeta>
+        <span data-testid='story-by'>
+          <StoryMetaElement color='#000'>By:</StoryMetaElement> {story.by}
+        </span>
+        <span data-testid='story-time'>
+          <StoryMetaElement color='#000'>Posted:</StoryMetaElement> {` `}
+          {story.time}
+        </span>
+      </StoryMeta>
+    </StoryWrapper>
   ) : null;
 };
--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { getStoryIds } from '../services/hnApi';
 import { Story } from '../components/Story';
+import {
+  GlobalStyle,
+  StoriesContainerWrapper
+} from '../styles/StoriesContainerStyles';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
@@ -10,5 +14,15 @@ export const StoriesContainer = () => {
     // eslint-disable-next-line
   }, []);
 
-  return storyIds.map(storyId => <Story key={storyId} storyId={storyId} />);
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id='stories-container'>
+        <h1>Hacker News Stories</h1>
+        {storyIds.map(storyId => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoriesContainerWrapper>
+    </Fragment>
+  );
 };
