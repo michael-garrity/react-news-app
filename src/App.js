71ec12ab6630b7cbc17839828dd0ccbec5e36b5e
mikergarrity <mikergarrity@gmail.com>
2019-10-09 19:50:21 -0500

    Story and StoriesContainer components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { getStoryIds } from './services/hnApi';
+import React from 'react';
+import { StoriesContainer } from './containers/StoriesContainer';
+import { storyUrl } from './services/hnApi';
 
-export const App = () => {
-  const [storyIds, setStoryIds] = useState([]);
-
-  useEffect(() => {
-    getStoryIds().then(data => setStoryIds(data));
-  }, []);
-
-  return <p>{JSON.stringify(storyIds)}</p>;
-};
+export const App = () => <StoriesContainer />;
---
+++ src/components/Story.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { getStory } from '../services/hnApi';
+
+export const Story = ({ storyId }) => {
+  const [story, setStory] = useState({});
+  useEffect(() => {
+    getStory(storyId).then(data => data && data.url && setStory(data));
+    // eslint-disable-next-line
+  }, []);
+  return story && story.url ? (
+    <div>
+      <a href={story.url}>
+        <p>{story.title}</p>
+      </a>
+      By: <p>{story.by}</p>
+      Posted: <p>{story.time}</p>
+    </div>
+  ) : null;
+};
